1a5368be7832ab5d0494a25dc5d8376185a1f233
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-20 14:57:48 -0700

    getting response back

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 var exphbs = require("express-handlebars");
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller.js");
 
