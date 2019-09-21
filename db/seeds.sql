45796070076f660dbf8cd4c6778cd7f5fe4c5472
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-20 21:27:48 -0700

    styled frontend

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,7 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('SIR LANSHALLOT', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('OLIVE-R TWIST', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('FRIED & PREJUDICE', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('SWEET DREAMS ARE MADE OF CHEESE', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('HAM-LET', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('LICENSE TO KALE', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('NEW YOLK CITY', false);
